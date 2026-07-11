cd7489aeb363c64dbf7ca7c9a8e316f3451f9ddd
sanjanasrinivas22 <sanjanasrinivas.cs23@bmsce.ac.in>
2026-07-11 22:36:44 +0530

    Time: 1 ms (99.98%), Space: 42.4 MB (90.87%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        long result = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            result = result * 10 + digit;
+            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int) result;
+    }
+}
